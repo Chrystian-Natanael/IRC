46b6a769cd850ed7abbbd9eb5a7c1331d96966e5
DanielSurf10 <daniel101surf@gmail.com>
2025-05-11 00:54:39 -0300

    :sparkles:feat: Implementação da classe Server e do main - Estrutura básica

---
+++ include/Server.hpp
@@ -0,0 +1,46 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#include <vector>
+#include <iostream>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <poll.h>
+
+class Server {
+private:
+	int							port;
+	int							server_socket_fd;
+	struct sockaddr				server_addr;
+	// std::vector<Client>			clients;
+	std::vector<struct pollfd>	fds;
+
+	// void	CloseFds();
+	// void	ClearClients(int fd);
+
+	static void	SetNonBlocking(int fd);
+
+	void		InitSocket();
+	void		SetSocketOptions();
+	void		BindSocket();
+	void		ListenSocket();
+	void		Poll();
+
+public:
+	Server();
+	Server(int port);
+	Server(const Server& src);
+	~Server();
+	Server& operator=(const Server& src);
+
+	void	ServerInit();
+	void	ServerLoop();
+	void	AcceptNewClient();
+	void	ReceiveData(int fd);
+};
+
+#endif
