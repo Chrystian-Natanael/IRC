ced2eb7a87661d6f3140045b490e2bf4e77f432d
Luana Ribeiro <l182300@dac.unicamp.br>
2025-06-09 23:54:16 -0300

    :sparkles: feat: comando NICK implementado e validado

--- src/Commands/NICK.cpp
+++ src/Commands/NICK.cpp
@@ -6,10 +6,30 @@ CommandPass::CommandNick(const std::string &command, const std::string &params) 
 CommandPass::~CommandNick(){}
 
 bool CommandNick::ValidateCommand(server &server, client &client) {
-    
+    if (this->_args.empty()) {
+        std::cerr << "NICK command requires a nickname." << std::endl;
+        // client.SendMessage(ERR_NONICKNAMEGIVEN(client.GetNickName()));
+        return false;
+    }
+    if (this->_args.length() < 4 || this->_args.length() > 9) {
+        std::cerr << "NICK command requires a nickname between 4 and 9 characters." << std::endl;
+        // client.SendMessage(ERR_ERRONEUSNICKNAME(client.GetNickName())); 
+        return false;
+    }
+    if (this->_args.alfanum() == false) {
+        std::cerr << "NICK command requires a nickname with alphanumeric characters only." << std::endl;
+        // client.SendMessage(ERR_ERRONEUSNICKNAME(client.GetNickName()));
+        return false;
+    }
+    for (std::vector<Client>::iterator it = server.clients.begin(); it != server.clients.end(); ++it) {
+        if (it->GetNickName() == this->_args) {
+            std::cerr << "Nickname já está em uso." << std::endl;
+            return false;
+    }
+    }
 }
 
 void CommandNick::Execute(client &client, server &server){
-    client.SetNickName = args;
-    server.clients.
+    client.SetNickName(this->_args)
+    client.login_state = USER;
 }
